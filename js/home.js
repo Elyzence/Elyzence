afcdd82deae070d5f20fc7a43d5fc4a5685575e1
Elyzence <158237077+Elyzence@users.noreply.github.com>
2024-02-01 11:33:17 +0800

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,6 @@
+document.getElementById("aboutme").addEventListener("click", function() {
+    window.location.href = "./aboutme.html"; 
+  });
+  document.getElementById("meditation").addEventListener("click", function() {
+    window.location.href = "./meditation.html"; 
+  });
